b2ed9538ebe1b72d587fed1cf1bd185df5c54e97
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2017-10-05 05:58:34 +0700

    Blog and comment blog and view and count comment

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :albums, only: [:index, :show]
   resources :blogs
   resources :abouts, only: [:index]
+  resources :comments, only: [:create]
 
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
